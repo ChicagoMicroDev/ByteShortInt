cbf60eb8d99aa7f81de9e63ec0a2f8ebff6af841
recklesskidjo3 <josephs409@gmail.com>
2018-12-24 13:27:44 -0600

    Taking some notes on Data Types

--- src/com/DevJds/Main.java
+++ src/com/DevJds/Main.java
@@ -3,6 +3,24 @@ package com.DevJds;
 public class Main {
 
     public static void main(String[] args) {
-	int myValue = 10000;
+//        The Minimum Value an Integer can be is
+//        Int has a width of 32
+        int myMinValue = -214783648;
+//        The Maximum Value an Integer can be is
+        int myMaxValue = 214783647;
+//                        Any Number to the right of the equal sign is a literal
+
+//        byte has a width of 8
+        byte myByteValue = -128;
+//        Byte is another Data type and can only store a value of -128 to 127
+
+//        short has a width of 16
+        short myShortValue = 30000;
+//        Short is another Data Type as well and can store values up -32768 to 37767
+
+//        Long has width of 64
+        long myLongValue = 100L;
+
     }
 }
+
